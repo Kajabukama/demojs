553ad6bfd46e99a582b5d0583bb4156f911a74a6
kajabukama <kajabukama@gmail.com>
2018-10-12 16:20:13 +0300

    initil commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Declared and initialized variables
+var numOne = 0;
+var numTwo = 0;
+var result = 0;
+
+// assign new values
+numOne = 25;
+numTwo = 30;
+result = 0;
+
+console.log(result);
+
+//Computing result
+result = numOne + numTwo;
+console.log(result);
+
+var firstName;
+var lastName;
+var fullName;
+var isHuman = true;
+
+if(isHuman){
+   console.log('Baraka is sleeping...')
+} else {
+   console.log('Baraka is laughing ...')
+}
+
+
+firstName = "Brian"
+lastName = "Christopher"
+fullName = firstName + "" + lastName;
+console.log(fullName)
+
+console.log("firstName")
---
+++ js/script_second.js
@@ -0,0 +1,35 @@
+var firstName;
+var middleName;
+var lastName;
+var fullName;
+console.log
+
+firstName="Brian"
+middleName="Honest"
+lastName="Christopher"
+fullName=firstName+" "+middleName+" "+lastName;
+console.log(fullName)
+console.log(fullName.length)
+
+
+var numOne;
+var numTwo;
+var numThree;
+var total;
+
+
+numOne= 0;
+numTwo=40;
+numThree=50;
+total= 0;
+console.log(total)
+
+total=numOne+numTwo+numThree;
+console.log(total)
+
+if(numOne===0){
+   console.log("numOne is equal to zero")
+}else{
+   console.log("numOne is not equal to zero")
+}
+
